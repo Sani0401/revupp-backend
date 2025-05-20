092f4b51de843cad9a10f8b3de9c95b8c051c6bd
Sani0401 <sanipatel0401@gmail.com>
2025-05-20 15:50:35 +0530

    Update: Fix call back

--- router.js
+++ router.js
@@ -2,10 +2,36 @@ import authRouter from "./router/auth.js";
 import express from "express";
 import crmRouter from "./router/crm.js";
 import coreRouter from "./router/core.js";
+import axios from "axios";
 const router = express.Router();
 
 router.use("/auth", authRouter);
 
 router.use("/crm", crmRouter);
 router.use("/core", coreRouter);
+
+router.get('/oauth2callback', async (req, res) => {
+   
+    const code = req.query.code;
+
+    // Exchange code for tokens
+    const params = new URLSearchParams();
+    params.append('code', code);
+    params.append('client_id', process.env.GOOGLE_CLIENT_ID);
+    params.append('client_secret', process.env.GOOGLE_CLIENT_SECRET);
+    params.append('redirect_uri', 'https://revupp-backend.onrender.com/api/oauth2callback');
+    params.append('grant_type', 'authorization_code');
+
+    try {
+        const tokenRes = await axios.post('https://oauth2.googleapis.com/token', params, {
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        });
+        // tokenRes.data contains access_token, refresh_token, etc.
+        console.log('tokenRes.data', tokenRes.data);
+        
+        res.json(tokenRes.data);
+    } catch (err) {
+        res.status(500).json({ error: 'Token exchange failed', details: err.response.data });
+    }
+});
 export default router;
